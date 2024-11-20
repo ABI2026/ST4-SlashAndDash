b7b5a356501f0c3bef957b3afed26d33d40a7750
MagnificentGoose <timich.stanke@gmail.com>
2024-11-20 11:53:16 +0100

    Fehler  entfernt

--- Slash-and-Dash/World.cpp
+++ Slash-and-Dash/World.cpp
@@ -5,14 +5,44 @@ World background;
 World forderground;
 World middleground;
 
-void World::push_back_backgroundTexture()
+void World::push_back_backgroundTexture(string b)
 {
+	background = b;
 }
 
-void World::push_back_foregroundTexture()
+void World::push_back_foregroundTexture(string f)
 {
+	forderground = f;
 }
 
-void World::push_back_middlegroundTexture()
+void World::push_back_middlegroundTexture(string m)
 {
+	middleground = m;
+}
+
+void World::loadTheWorld(int a, int b, int c)
+{
+	sf::Texture fordergroundT;
+	sf::Texture middlegroundT;
+	sf::Texture backgroundT;
+	
+	if (!fordergroundT.loadFromFile(forderground[a])) {
+		std::cout << "Fehler beim Laden des forderground!" << endl;
+		return -1;
+	}
+	if (!middlegroundT.loadFromFile(middleground[b])) {
+		std::cout << "Fehler beim Laden des middlegrounds!" << endl;
+		retrun - 1;
+	}
+	if (!backgroundT.loadFromFile(background[c])) {
+		std::cout << "Fehler beim Laden des backgrounds!" << endl;
+	}
+	
+	sf::Sprite forground;
+	sf::Sprite midground;
+	sf::Sprite hintergrund;
+
+	forground.setTexture(fordergroundT);
+	midground.setTexture(middlegroundT);
+	hintergrund.setTexture(backgroundT);
 }
--- Slash-and-Dash/World.h
+++ Slash-and-Dash/World.h
@@ -7,11 +7,12 @@ private:
 	std::vector<sf::Texture> backgroundTextures;
 	std::vector<sf::Texture> foregroundTextures;
 	std::vector<sf::Texture> middlegroundTextures;
-public:
-	void push_back_backgroundTexture();
-	void push_back_foregroundTexture();
-	void push_back_middlegroundTexture();
 	
+public:
+	void push_back_backgroundTexture(string);
+	void push_back_foregroundTexture(string);
+	void push_back_middlegroundTexture(string);
+	int getTheWorld(int, int, int);
 };
 
 
