63a11bb4b28803cad0c8809fa9634f5b1618d666
MagnificentGoose <timich.stanke@gmail.com>
2024-11-29 13:49:59 +0100

    Durchsichtige Nebel hinzugefügt

--- Slash-and-Dash/World.cpp
+++ Slash-and-Dash/World.cpp
@@ -2,27 +2,29 @@
 #include <iostream>
 
 World::World(){
-	push_back_backgroundTexture("Assets/Texture/Sprite-0001background.png");
+	push_back_backgroundTexture("Assets/Texture/Sprite-0001background.png");  // Texturen ändern 
 	push_back_foregroundTexture("Assets/Texture/trees.png");
 	push_back_middlegroundTexture("Assets/Texture/mountains.png");
-	loadTheWorld(0,0,0);
+	push_back_durchsichtigVorne("Assets/Texture/Nebel-Hell.png");
+	push_back_durchsichtigHinten("Assets/Texture/Nebel-Dunkel.png");
+	loadTheWorld(0,0,0,0,0);
 }
-
-World::World(sf::Vector2u windowSize)//Texturengr��e richtig skalieren 
-{
-
-	sf::Vector2u texturGrossefront = fordergroundT.getSize();
-	sf::Vector2u texturGrossemid = middlegroundT.getSize();
-	sf::Vector2u texturGrosseback = backgroundT.getSize();
-
-	 //scaleX = static_cast<float>(windowSize.x) / texturGrosseback.x;
-	 //scaleY = static_cast<float>(windowSize.y) / texturGrosseback.y;
-
-	 zentrierenX = windowSize.x;
-	 zentrierenY = windowSize.y;
-
-}
-
+// wahrscheinlich unnötig
+//World::World(sf::Vector2u windowSize)//Texturengr��e richtig skalieren 
+//{
+//
+//	sf::Vector2u texturGrossefront = fordergroundT.getSize();
+//	sf::Vector2u texturGrossemid = middlegroundT.getSize();
+//	sf::Vector2u texturGrosseback = backgroundT.getSize();
+//
+//	 //scaleX = static_cast<float>(windowSize.x) / texturGrosseback.x;
+//	 //scaleY = static_cast<float>(windowSize.y) / texturGrosseback.y;
+//
+//	 zentrierenX = windowSize.x;
+//	 zentrierenY = windowSize.y;
+//
+//}
+// Grafiken hinzufügen
 void World::push_back_backgroundTexture(std::string b)
 {
 	backgroundTextures.push_back(b);
@@ -38,7 +40,18 @@ void World::push_back_middlegroundTexture(std::string m)
 	middlegroundTextures.push_back(m);
 }
 
-void World::loadTheWorld(int a, int b, int c)
+void World::push_back_durchsichtigVorne(std::string durch_Vorne)
+{
+	durchsicht_nah_Texture.push_back(durch_Vorne);
+}
+
+void World::push_back_durchsichtigHinten(std::string durch_Hinten)
+{
+	durchsicht_weit_Texture.push_back(durch_Hinten);
+}
+// ende Methoden Grafiken hinzufügen
+// Grafiken zeigen laden
+void World::loadTheWorld(int a, int b, int c, int d, int e)
 {
 
 	if (!fordergroundT.loadFromFile(foregroundTextures[a])) {
@@ -53,12 +66,41 @@ void World::loadTheWorld(int a, int b, int c)
 		//std::cout << "Fehler beim Laden des backgrounds!" << endl;
 		//return 0;
 	}
+	if (!durchsichtig_nah.loadFromFile(durchsicht_nah_Texture[d])) {
+		//std::cout << "Fehler beim Laden des backgrounds!" << endl;
+		//return 0;
+	}
+	if (!durchsichtig_weit.loadFromFile(durchsicht_weit_Texture[e])) {
+		//std::cout << "Fehler beim Laden des backgrounds!" << endl;
+		//return 0;
+	}
 
-
+	durchsichtW.setTexture(durchsichtig_weit);
+	durchsichtH.setTexture(durchsichtig_nah);
 	forground.setTexture(fordergroundT);
 	midground.setTexture(middlegroundT);
 	hintergrund.setTexture(backgroundT);
 }
+//Parallax Effekt Funktion 
+void World::paraMove()
+{
+
+}
+
+//Parallax Effekt funktion
+//void World::moveParaEffekt()
+//{
+//	sf::Clock clock;
+//	float elapsedTime = clock.getElapsedTime().asSeconds();
+//	while () {
+//		if ()
+//
+//
+//
+//
+//	}
+//}
+
 // update Grafiken abh�ngig von dem Fenster
 
 void World::render(sf::RenderTarget* t){
@@ -66,14 +108,16 @@ void World::render(sf::RenderTarget* t){
 
 	//sf::Vector2u fensterGroesse = window.getSize();  //fenstergr��e bestimmen
 	//sf::Vector2u texturGroesse = textur.getSize();  //texturgr��e bestimmen
-
-
+	
 	t->draw(hintergrund);
+	//t->draw(durchsichtW); //zwie layers sind zu viel
 	t->draw(midground);
+	//t->draw(durchsichtH);
 	t->draw(forground);
 	
-	hintergrund.setPosition(0,0);
-	hintergrund.setScale(0.77,0.6);
+
+	hintergrund.setPosition(-10,0);
+	hintergrund.setScale(0.79,0.6);
 	midground.setPosition(0, 50);
 	midground.setScale(0, 0);
 	forground.setPosition(0, 0);
--- Slash-and-Dash/World.h
+++ Slash-and-Dash/World.h
@@ -13,21 +13,32 @@ private:
 	std::vector<std::string> backgroundTextures;
 	std::vector<std::string> foregroundTextures;
 	std::vector<std::string> middlegroundTextures;
-	
+	std::vector<std::string> durchsicht_nah_Texture;
+	std::vector<std::string> durchsicht_weit_Texture;
+
+	sf::Texture durchsichtig_weit;
+	sf::Texture durchsichtig_nah;
 	sf::Texture fordergroundT;
 	sf::Texture middlegroundT;
 	sf::Texture backgroundT;
 
+	sf::Sprite durchsichtW;
+	sf::Sprite durchsichtH;
 	sf::Sprite forground;
 	sf::Sprite midground;
 	sf::Sprite hintergrund;
 public:
 	World();
-	World(sf::Vector2u windowSize);
+	/*World(sf::Vector2u windowSize);*/
 	void push_back_backgroundTexture(std::string);
 	void push_back_foregroundTexture(std::string);
 	void push_back_middlegroundTexture(std::string);
-	void loadTheWorld(int, int, int);
+	void push_back_durchsichtigVorne(std::string);
+	void push_back_durchsichtigHinten(std::string);
+	void loadTheWorld(int, int, int, int, int);
+	void paraMove();
+	//void moveParaEffekt();
+
 	
 	void render(sf::RenderTarget* t);
 };
